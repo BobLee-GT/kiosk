c4ee71114800e4201dfcd91f0cd1ac07f58b3aad
BobLee <lethanhtu18031999@gmail.com>
2024-02-01 14:17:55 +0700

    Test Printer

--- lib/modules/login/controller/login_controller.dart
+++ lib/modules/login/controller/login_controller.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:medipay/my_app.dart';
+import 'package:medipay/my_app01.dart';
 import 'package:medipay/routes/app_pages.dart';
 
 class LoginController extends GetxController {
 
   void onTapCCCD(){
-    Get.toNamed(Routes.SCANID);
+    // Get.toNamed(Routes.SCANID);
+    Navigator.of(Get.context!).push(MaterialPageRoute(builder: (context) => MyApp1()));
   }
 
   void onTapBHYT(){
